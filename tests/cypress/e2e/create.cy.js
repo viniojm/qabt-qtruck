333540a5a487a0d23ea2687ba51a4bb2f08421ac
Vinicius Oliveira <viniciusoliveira2807@hotmail.com>
2022-08-28 20:30:09 -0300

    criando caso de foodtruck duplicado

--- tests/cypress/e2e/create.cy.js
+++ tests/cypress/e2e/create.cy.js
@@ -12,8 +12,8 @@ describe('Recomendação', () => {
         }
 
         const foodtruck = {
-            latitude: '-23.593604408436704',
-            longitude: '-46.684293150901794',
+            latitude: '-23.569808847808314',
+            longitude: '-46.71635627746583',
             name: 'Tienda Del Chavo',
             details: 'O melhor lugar para tomar um suco gelado e conversar com os amigos',
             opening_hours: 'das 14h as 20h',
@@ -27,5 +27,47 @@ describe('Recomendação', () => {
         CreatePage.form(foodtruck)
         CreatePage.submit()
         CreatePage.modal.haveText('Food truck cadastrado com sucesso!')
+    })
+
+    it ('não deve cadastrar foodtruck com o nome duplicado', ()=>{
+        const user = {
+            name: 'Vinicius',
+            instagram: '@vinipiurx',
+            password: 'Aaaaaa1!'
+        }
+
+        const foodtruck = {
+            latitude: '-23.569808847808314',
+            longitude: '-46.71635627746583',
+            name: 'Tienda Del Chavo',
+            details: 'Um bom pastel de queijo',
+            opening_hours: 'das 14h as 20h',
+            open_on_weekends: true
+        }
+
+        cy.uiLogin(user)
+        mapPage.createLink()
+        CreatePage.form(foodtruck)
+        CreatePage.submit()
+        CreatePage.modal.haveText('Esse food fruck já foi cadastrado!')
+    })
+
+    it ('todos os campos são obrigatórios', ()=>{
+        const user = {
+            name: 'Benson',
+            instagram: '@benson',
+            password: 'Aaaaaa1!'
+        }
+
+        const foodtruck = {
+        }
+
+        cy.apiCreateUser(user)
+        cy.uiLogin(user)
+
+        mapPage.createLink()
+        CreatePage.form(foodtruck)
+        CreatePage.submit()
+        CreatePage.modal.haveText('O campos nome, descrição e horário de funcionamento devem ser informados para recomendar um food truck!')
     })
 })
--- tests/cypress/support/pages/Create/index.js
+++ tests/cypress/support/pages/Create/index.js
@@ -9,9 +9,9 @@ class CreatePage {
 
     form(foodtruck) {
         cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
-        cy.get('input[name=name]').type(foodtruck.name)
-        cy.get('textarea[name=details]').type(foodtruck.details)
-        cy.get('input[name=opening-hours]').type(foodtruck.opening_hours)
+        if (foodtruck.name) cy.get('input[name=name]').type(foodtruck.name)
+        if (foodtruck.details) cy.get('textarea[name=details]').type(foodtruck.details)
+        if (foodtruck.opening_hours) cy.get('input[name=opening-hours]').type(foodtruck.opening_hours)
 
         cy.contains('button', foodtruck.open_on_weekends ? 'Sim' : 'Não')
             .click()
